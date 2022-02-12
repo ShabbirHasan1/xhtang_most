829e95b104f9933ae71e6722e517d748280989db
Xianghao Tang <t-x.h@163.com>
2022-02-12 15:07:47 +0800

    Add optimization options

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@ while true; do
     if [[ $1 == *.py ]]; then
         python3.8 $1
     elif [[ $1 == *.cpp ]]; then
-        g++ $1 -o $1.out -O3 -std=c++17 -lpthread -lm -Wall
+        rm -f $1.out
+        g++ $1 -o $1.out -O3 -std=c++17 -lpthread -Wall -Wextra -Werror -march=native -mtune=native
         ./$1.out
     else
         $1

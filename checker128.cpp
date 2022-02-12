da37aecfb774da95dc4dcc44b4abdc4fec17c9b4
Xianghao Tang <t-x.h@163.com>
2022-02-12 19:23:32 +0800

    implement checker

---
+++ checker128.cpp
@@ -0,0 +1,14 @@
+#include <cstdint>
+
+#define factor_t __uint128_t
+
+factor_t M;
+void init()
+{
+    const char M2[] = "104648257118348370704723099";
+    M = 0;
+    for (const char *c = M2; *c != '\0'; ++c)
+        M = M * 10 + factor_t(*c - '0');
+}
+
+#include "checker.h"

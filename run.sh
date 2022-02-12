7c0e3693d31b973e7952545c8bfa8bcbe20415a7
Xianghao Tang <t-x.h@163.com>
2022-02-12 15:18:52 +0800

    Add a bin dir

--- run.sh
+++ run.sh
@@ -2,9 +2,10 @@ while true; do
     if [[ $1 == *.py ]]; then
         python3.8 $1
     elif [[ $1 == *.cpp ]]; then
-        rm -f $1.out
-        g++ $1 -o $1.out -O3 -std=c++17 -lpthread -Wall -Wextra -Werror -march=native -mtune=native
-        ./$1.out
+        mkdir -p ./bin
+        rm -f ./bin/$1.out
+        g++ $1 -o ./bin/$1.out -O3 -std=c++17 -lpthread -Wall -Wextra -Werror -march=native -mtune=native
+        ./bin/$1.out
     else
         $1
     fi

9fa3cf85bd469d113c7a79a90914db1a05d2399e
Xianghao Tang <t-x.h@163.com>
2022-02-12 11:31:00 +0800

    update run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+while true; do
+    if [[ $1 == *.py ]]; then
+        python3.8 $1
+    elif [[ $1 == *.cpp ]]; then
+        g++ $1 -o $1.out -O3 -std=c++17
+        ./$1.out
+    else
+        $1
+    fi
+    sleep 1
+done

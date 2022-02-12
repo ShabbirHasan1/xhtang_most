12944894c6edd5b55bd313ee8fc55007cdbccfc2
Xianghao Tang <t-x.h@163.com>
2022-02-12 22:46:46 +0800

    log data

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
+mkdir -p ./log
+mkdir -p ./bin
+
 while true; do
     if [[ $1 == *.py ]]; then
-        python3.8 $1
+        python3.8 $1 | tee -a ./log/$1.log
     elif [[ $1 == *.cpp ]]; then
-        mkdir -p ./bin
         rm -f ./bin/$1.out
         g++ $1 -o ./bin/$1.out -O3 -std=c++17 -lpthread -Wall -Wextra -Werror -march=native -mtune=native
-        ./bin/$1.out
+        ./bin/$1.out | tee -a ./log/$1.log
     else
         $1
     fi

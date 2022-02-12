df7a557780c211ed7e2b04966e6ae1d3a267a8cb
Xianghao Tang <t-x.h@163.com>
2022-02-12 11:34:37 +0800

    add linking options

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ while true; do
     if [[ $1 == *.py ]]; then
         python3.8 $1
     elif [[ $1 == *.cpp ]]; then
-        g++ $1 -o $1.out -O3 -std=c++17
+        g++ $1 -o $1.out -O3 -std=c++17 -lpthread -lm
         ./$1.out
     else
         $1

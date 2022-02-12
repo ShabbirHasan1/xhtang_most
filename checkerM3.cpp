6dc109786f67afbbb6d698ec0419187b94a966f8
Xianghao Tang <t-x.h@163.com>
2022-02-12 22:36:01 +0800

    implement checkerM3

---
+++ checkerM3.cpp
@@ -0,0 +1,20 @@
+#include <cstdint>
+#include <climits>
+#include <cassert>
+#include <algorithm>
+
+#define factor_t uint64_t
+#define FACTOR
+
+const int n_factor = 3;
+factor_t factors[n_factor] = {
+    500000000000000021ULL,
+    500000000000000107ULL,
+    500000000000000131ULL,
+};
+
+void init()
+{
+}
+
+#include "checkerV2.h"

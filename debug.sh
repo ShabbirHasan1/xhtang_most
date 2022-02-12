2082e769ea47e2cd0eb9381ce92a6d080366196a
Xianghao Tang <t-x.h@163.com>
2022-02-12 20:12:26 +0800

    Add debug script

---
+++ debug.sh
@@ -0,0 +1,7 @@
+if [[ $1 == *.cpp ]]; then
+    mkdir -p ./bin
+    rm -f ./bin/$1.dbg
+    g++ $1 -o ./bin/$1.dbg -g -std=c++17 -lpthread -Wall -Wextra -Werror && gdb ./bin/$1.dbg
+else
+    gdb $1
+fi

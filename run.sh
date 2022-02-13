e28e7a9deb7dc1f632027e2bb84313b32b85640e
Xianghao Tang <t-x.h@163.com>
2022-02-13 14:40:30 +0800

    remove log

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@ mkdir -p ./bin
 
 while true; do
     if [[ $1 == *.py ]]; then
-        python3.8 $1 | tee -a ./log/$1.log
+        python3.8 $1 #| tee -a ./log/$1.log
     elif [[ $1 == *.cpp ]]; then
         rm -f ./bin/$1.out
         g++ $1 -o ./bin/$1.out -O3 -std=c++17 -lpthread -Wall -Wextra -Werror -march=native -mtune=native
-        ./bin/$1.out | tee -a ./log/$1.log
+        ./bin/$1.out #| tee -a ./log/$1.log
     else
         $1
     fi

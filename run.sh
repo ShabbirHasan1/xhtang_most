6814acb754da5cfb95e2059b3a499cabb3720b68
Xianghao Tang <t-x.h@163.com>
2022-02-13 20:12:33 +0800

    update scripts

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 mkdir -p ./log
 mkdir -p ./bin
 
-if [[ $USER == "root" ]]; then
+nice_command=$2
+if [[ $nice_command == "" && $USER == "root" ]]; then
     nice_command='nice -n -20'
-else
-    nice_command=''
 fi
 
 while true; do
@@ -15,7 +14,7 @@ while true; do
         g++ $1 -o ./bin/$1.out -O3 -std=c++17 -lpthread -Wall -Wextra -Werror -march=native -mtune=native
         $nice_command ./bin/$1.out #| tee -a ./log/$1.log
     else
-        $1
+        $nice_command $1
     fi
     sleep 1
 done
--- setup.sh
+++ setup.sh
@@ -3,3 +3,8 @@ apt install -y git tmux htop
 ssh-keygen -t rsa
 git clone ssh://git@bitbucket.pinghu.tech/~xhtang/low-latency-trading-101.git
 python3 -m pip install --upgrade requests
+
+# centos 7.9
+yum install git tmux htop centos-release-scl devtoolset-10 -y
+echo "source /opt/rh/devtoolset-10/enable" >> /etc/profile
+source /etc/profile

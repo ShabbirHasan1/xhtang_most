e069ea234aceac3fdcb546f8725db9b6ad471c2e
Xianghao Tang <t-x.h@163.com>
2022-02-17 19:53:00 +0800

    add run command

--- setup.sh
+++ setup.sh
@@ -8,3 +8,5 @@ python3 -m pip install --upgrade requests
 yum install git tmux htop centos-release-scl devtoolset-10 -y
 echo "source /opt/rh/devtoolset-10/enable" >> /etc/profile
 source /etc/profile
+
+bash run.sh a.cpp 'chrt -f 99 taskset -c 0'

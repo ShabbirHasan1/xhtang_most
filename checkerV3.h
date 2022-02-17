6e6c55e5acad2069f6cdd3b94c17564d500b2c1f
Xianghao Tang <t-x.h@163.com>
2022-02-17 17:34:25 +0800

    fix non-blocking issues

--- checkerV3.h
+++ checkerV3.h
@@ -96,7 +96,6 @@ void connect_to_port(int socket_fd, int port)
     rt_assert_eq(inet_pton(AF_INET, SERVER_IP, &addr_v4->sin_addr), 1);
 
     rt_assert_eq(connect(socket_fd, (struct sockaddr *)addr_v4, sizeof(*addr_v4)), 0);
-    rt_assert_eq(fcntl(socket_fd, F_SETFL, fcntl(socket_fd, F_GETFL) | O_NONBLOCK), 0);
 }
 
 // 生成提交所使用的TCP连接池
@@ -128,6 +127,7 @@ void gen_submit_fd()
 
             // connect
             connect_to_port(socket_fd, 10002);
+            rt_assert_eq(fcntl(socket_fd, F_SETFL, fcntl(socket_fd, F_GETFL) | O_NONBLOCK), 0);
             submit_fds[i] = socket_fd;
         }
 }
@@ -414,6 +414,8 @@ int main()
 
     // main loop
     {
+        rt_assert_eq(fcntl(socket_fd, F_SETFL, fcntl(socket_fd, F_GETFL) | O_NONBLOCK), 0);
+
         pollfd poll_info;
         poll_info.fd = socket_fd;
         poll_info.events = POLLIN;

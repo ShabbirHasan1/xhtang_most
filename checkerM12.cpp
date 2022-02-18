c269193edb155d82312a679fa355cc93b33ef030
Xianghao Tang <t-x.h@163.com>
2022-02-19 02:54:55 +0800

    stop M2 calc

--- checkerM12.cpp
+++ checkerM12.cpp
@@ -8,14 +8,14 @@ void init()
         // 2022021721441 * 10
         checkers[n_checkers++] = new Checker<uint64_t, true>(2022021721441);
     }
-    {
-        printf("init M2 checker\n");
-        typedef __uint128_t factor_t;
-        factor_t M = 0;
-        char m[] = "104648257118348370704723119";
-        for (size_t i = 0; i < sizeof(m); ++i)
-            M = M * 10 + m[i] - '0';
-        checkers[n_checkers++] = new Checker<factor_t>(M);
-    }
+    // {
+    //     printf("init M2 checker\n");
+    //     typedef __uint128_t factor_t;
+    //     factor_t M = 0;
+    //     char m[] = "104648257118348370704723119";
+    //     for (size_t i = 0; i < sizeof(m); ++i)
+    //         M = M * 10 + m[i] - '0';
+    //     checkers[n_checkers++] = new Checker<factor_t>(M);
+    // }
     N_CHECKER = n_checkers;
 }

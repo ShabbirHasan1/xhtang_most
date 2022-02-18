b5b41fa62ae02c36a8483bf2671a2f4e060f6bad
Xianghao Tang <t-x.h@163.com>
2022-02-19 01:59:12 +0800

    separate checkers

--- checkerV5_M12.cpp
+++ checkerV5_M12.cpp
@@ -98,7 +98,7 @@ struct Submitter
     int headers_n[N + 10];
 
     // 提交所使用的TCP连接池
-    static constexpr int SUBMIT_FD_N = 8;
+    static constexpr int SUBMIT_FD_N = 6;
     int next_submit_fd = 0;
     int submit_fds[SUBMIT_FD_N];
 
@@ -186,7 +186,7 @@ struct Submitter
         if (ans_cnt > 0)
         {
 #ifdef DEBUG
-            for(int i = 0; i < ans_cnt; ++i)
+            for (int i = 0; i < ans_cnt; ++i)
             {
                 {
                     printf("%.6lf %d ", sent_times[i] - received_time, ans_stage[i]);
@@ -638,28 +638,6 @@ int main()
         for (size_t i = 0; i < sizeof(m); ++i)
             M = M * 10 + m[i] - '0';
         checkers[n_checkers++] = new Checker<factor_t>(M);
-    }
-    {
-        printf("init M3 checker\n");
-        checkers[n_checkers++] = new Checker<uint64_t>(500000000000000147ULL);
-    }
-    {
-        printf("init M4 checker\n");
-        const int n_factor = 3;
-        const int factor_base[] = {3, 7, 11};
-        const int powers[] = {35, 20, 15}; // {50, 30, 20};
-        typedef uint64_t factor_t;
-        factor_t factors[n_factor];
-        for (int i_factor = 0; i_factor < n_factor; ++i_factor)
-        {
-            factors[i_factor] = 1;
-            for (int j = 0; j < powers[i_factor]; ++j)
-            {
-                factors[i_factor] *= factor_base[i_factor];
-                assert(factors[i_factor] < UINT64_MAX / 15);
-            }
-        }
-        checkers[n_checkers++] = new Checker<factor_t>(factors[2]);
     }
     N_CHECKER = n_checkers;
 

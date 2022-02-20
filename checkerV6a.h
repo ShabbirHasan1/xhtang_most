fdc30ccbd286fcb14afea5d3ed871043a68dc00d
Xianghao Tang <t-x.h@163.com>
2022-02-20 11:56:50 +0800

    reduce memory usage

--- checkerV6a.h
+++ checkerV6a.h
@@ -284,7 +284,7 @@ struct Sqr<uint32_t>
 template <typename K, typename V>
 struct SmallHash
 {
-    static constexpr size_t CAPACITY = N * N;
+    static constexpr size_t CAPACITY = N * N / 2;
     V table[CAPACITY];
 
     SmallHash()
